bcf6378890c8e8ccaf5f7f570a59cbebcb264f61
rgwozdz <rgwozdz@spatialdev.com>
2013-10-25 09:20:16 -0700

    Refactor taxonomy filters to more generic facet-filter. Adjust quick-cluster utility that adds colors to facet values.

--- q-cluster-leaflet-layer.js
+++ q-cluster-leaflet-layer.js
@@ -536,8 +536,7 @@ QClusterLeafletLayer.makeTaxClassifications = function(taxClassData, opts) {
 		 	'classificationArr': [],
 		 	'otherColor': otherColor
 		 	
-		};
-		
+		};		
 		
 		// Loop thru each classification
 		_.each(taxonomy.classifications, function(classification, i){
@@ -578,4 +577,65 @@ QClusterLeafletLayer.makeTaxClassifications = function(taxClassData, opts) {
 	
 	return {'objArray': collection, 'keyValues' : keyValues};
 	
+};
+
+QClusterLeafletLayer.colorFacetValues = function(facetId, facetName, facetValues, propMap, opts) {
+	
+	var color,
+		options,
+		index,
+		colorPalette,
+		maxColors,
+		otherColor,
+		facet;
+	
+	options = opts || {};
+			
+	colorPalette = options.colorPalette || ['#8b722c','#e7dfc7','#040707','#c96228','#80adc0','#a19788','#ddecf2','#9e0000','#03671f','#8e2b5c','#e13066','#5c8276','#efa0cb','#62517b','#2c688b','#56c2a7','#e1df2f','#ed3333','#e69890','#545454'];	
+	
+	maxColors = options.maxColors || 50;
+	
+	otherColor = options.otherColor || '#666666';
+	
+		facet = {
+			
+			'id': facetId,
+		 	'name': facetName,
+		 	'values_keyVal' : {},
+		 	'values_arr': [],
+		 	'otherColor': otherColor
+		 	
+		};	
+		
+		// Loop thru each classification
+		_.each(facetValues, function(fVal, i){
+			
+			var facetValue = {
+				'id': fVal[propMap.id],
+				'name': fVal[propMap.name], 
+				'color': fVal[propMap.color], 
+			};
+			
+			if(typeof facetValue.color === 'undefined' || facetValue.color === null) {
+				
+				if (i > maxColors - 1) {
+					facetValue.color = otherColor;
+				
+				} else if (i > colorPalette.length - 1) {
+					
+					index = (i % colorPalette.length) - 1;
+					facetValue.color = colorPalette[index];
+				
+				} else {
+					
+					facetValue.color = colorPalette[i];					
+				}				
+			}
+			
+			facet.values_keyVal[facetValue.id] = facetValue;
+			
+			facet.values_arr.push(facetValue);
+		});
+
+	return facet;
 };
