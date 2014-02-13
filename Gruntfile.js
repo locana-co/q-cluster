2186fdd682ff51e23c32fe434303f382552d2ed1
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-02-13 10:30:04 -0800

    no need to run copy anymore

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['uglify', 'concat', 'copy']);
+    grunt.registerTask('default', ['uglify', 'concat']);
 	
 	
 };
