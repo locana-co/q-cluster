5e79b18e23153d3d7e71fb93b86d138ad39f584a
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-02-13 10:26:45 -0800

    updated gruntfile and package.json, stopped tracking node_modules dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,21 +4,21 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-		uglify: {
-			options: {
-		        report: 'min',
-		        sourceMap: 'lib/q-cluster.map.js',
-		        sourceMapPrefix: 1,
-		        sourceMappingURL: 'q-cluster.map.js'
-		     },
-		    build: {
-		        src: [
-                    'src/clustering.js', 'src/point-clusterer.js', 'src/make-donuts.js', 'src/utils.js'
-		        ],
-		        dest: 'lib/q-cluster.min.js'
-		    }
-		},
-        
+    		uglify: {
+    			  options: {
+    		        report: 'min',
+    		        sourceMap: 'lib/q-cluster.map.js',
+    		        sourceMapPrefix: 1,
+    		        sourceMappingURL: 'q-cluster.map.js'
+    		    },
+    		    build: {
+    		        src: [
+                        'src/clustering.js', 'src/point-clusterer.js', 'src/make-donuts.js', 'src/utils.js'
+    		        ],
+    		        dest: 'lib/q-cluster.min.js'
+    		    }
+    		},
+
         concat: {
 
             dist: {
@@ -26,13 +26,10 @@ module.exports = function(grunt) {
               dest: 'lib/q-cluster.js',
             }
         },
-        copy: {
-          main: {
-            files: [
-              // includes files within path
-              {expand: true, flatten: true,src: ['lib/**'], dest: 'demo/js/q-cluster/', filter: 'isFile'}
-            ]
-          }
+
+        watch: {
+          files: ['src/*', 'css/*', 'demo/index.html'],
+          tasks: ['concat', 'uglify']
         }
 		
     });
@@ -42,6 +39,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['uglify', 'concat', 'copy']);
